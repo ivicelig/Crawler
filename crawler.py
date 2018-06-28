5c46ed20508da9638921210a2ce73d96097f6eb9
Ivica <ivicelig@gmail.com>
2018-06-28 19:17:58 +0200

    First

---
+++ crawler.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun 28 18:41:42 2018
+
+@author: Ivica
+"""
+from html.parser import HTMLParser
+from urllib.request import urlopen
+from urllib import parse
