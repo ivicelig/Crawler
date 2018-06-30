c0b45dc5fa01c42c7531d02f20f33d3139299ad3
Ivica <ivicelig@gmail.com>
2018-06-30 23:57:09 +0200

    Fixed bugs; added comments

--- crawler.py
+++ crawler.py
@@ -11,14 +11,16 @@ import sys
 import threading
 import time
 import youtube_api
+
 class LinkParser(HTMLParser):
+    #If <a href='.....'><a/> is found then read append link to links array
     def handle_starttag(self,tag,attrs):
         if tag == 'a':
             for(key, value) in attrs:
                 if key == 'href':
-                    newUrl = parse.urljoin(self.baseUrl, value)
-                    
+                    newUrl = parse.urljoin(self.baseUrl, value) 
                     self.links = self.links + [newUrl]
+    #Open page and read its HTML content; self.feed to start the appending-of-links process               
     def getLinks(self, url):
         self.links = []
         self.baseUrl = url
@@ -32,6 +34,8 @@ class LinkParser(HTMLParser):
             return htmlString, self.links
         else:
             return "",[]
+#Thread class that uses youtube_api function to get all comments from URL and shows authors and comments
+#if they contain specific word
 class YTCommentCheck(threading.Thread):
     def __init__(self,URL,word,binarySemaphore):
         self.binarySemaphore = binarySemaphore
@@ -43,7 +47,7 @@ class YTCommentCheck(threading.Thread):
         ytApi.get_video_comments(self.url)
         comments = ytApi.comments
         replies = ytApi.replies
-        
+        #Search comment array
         for i in comments:
             if i[0].find(self.word)>-1 or i[1].find(self.word)>-1:
                 self.binarySemaphore.acquire()
@@ -54,9 +58,11 @@ class YTCommentCheck(threading.Thread):
                 self.binarySemaphore.acquire()
                 print("YT: The word", self.word, "was found at", self.url," Reply autor:",i[2]," Reply comment:",i[3])
                 self.binarySemaphore.release()
+                
 class CrawlerThread(threading.Thread):     
     base_video_url_ssh = 'https://www.youtube.com/watch?v='
     base_video_url = 'http://www.youtube.com/watch?v='
+    maxThreadCount = 1
     def __init__(self, binarySemaphore,url,word):
          self.binarySemaphore = binarySemaphore
          self.url = url
@@ -64,6 +70,7 @@ class CrawlerThread(threading.Thread):
          
          threading.Thread.__init__(self)
     def run(self):
+            youtubeVideos = []
             ytThreads = []
             print("Thread started!")
             pagesToVisit = [self.url]
@@ -79,30 +86,46 @@ class CrawlerThread(threading.Thread):
                     parser = LinkParser()
                     print(url)
                     data, links = parser.getLinks(url)
+                    
+                    #If link is youtube video
                     if(url.startswith(self.base_video_url) or url.startswith(self.base_video_url_ssh)):
-                        ytThread = YTCommentCheck(url,self.word,self.binarySemaphore)   
-                        ytThread.start()
-                        ytThreads.append(ytThread)
-                    elif data.find(self.word)>-1:
+                        #If thread array is empty
+                        if(len(ytThreads)==0):
+                            ytThread = YTCommentCheck(url,self.word,self.binarySemaphore)
+                            ytThread.start()
+                            ytThreads.append(ytThread)
+                        #If thread array is not empty 
+                        else:
+                            #then check if thread count is above certain value add in temp array of URLS
+                            if (len(ytThreads)>self.maxThreadCount):
+                                youtubeVideos = youtubeVideos + [url]
+                            #If not then get URL from temp URL (youtubeVideos) array
+                            else:
+                                youtubeVideos = youtubeVideos + [url]
+                                ytThread = YTCommentCheck(youtubeVideos[0],self.word,self.binarySemaphore)   
+                                youtubeVideos = youtubeVideos[1:]
+                                ytThread.start()
+                                ytThreads.append(ytThread)
+                    #If page contains specific word, show message to user
+                    if data.find(self.word)>-1:
                         self.binarySemaphore.acquire()
                         print("The word", self.word, "was found at", url)
             
-                    #print(set(pagesToVisit + links))
+                    #Add all links found on page in pagesToVisit array
                     pagesToVisit = list(set(pagesToVisit + links))
                     print("Success!")
                     self.binarySemaphore.release()
             
                 except Exception as e:
-                        print ("Exception: "+ str(e))
+                        type, value, traceback = sys.exc_info()
+                        print ("Exception: "+ str(e) + type, value, traceback)
             for i in ytThreads:
                 i.join()
                 
 run_event = threading.Event()
 
 binarySemaphore = threading.Semaphore(1)    
-#ytThread = YTCommentCheck("https://www.youtube.com/watch?v=tQ6PRUgtCuM","amazed",binarySemaphore)   
-#ytThread.start()
-#ytThread.join() 
+
 thread1 = CrawlerThread(binarySemaphore,"https://www.youtube.com/watch?v=8xpbuSZGA4c","only")
 thread2 = CrawlerThread(binarySemaphore,"http://www.facebook.com","bank")
 thread1.start()
--- youtube_api.py
+++ youtube_api.py
@@ -14,25 +14,27 @@ class YouTubeApi():
     def get_video_comments(self, videoURL):
         
        
-            
+        #Fill comments and replies arrays with data from API json response
         def load_comments(self):
             for item in mat["items"]:
                 comment = item["snippet"]["topLevelComment"]
                 author = comment["snippet"]["authorDisplayName"]
                 text = comment["snippet"]["textDisplay"]
+                #Save values in comment array
                 self.comments = self.comments + [[author,text]]
             
-                #print("Comment by {}: {}".format(author, text))
+                
                 if 'replies' in item.keys():
                     for reply in item['replies']['comments']:
                         rauthor = reply['snippet']['authorDisplayName']
                         
                         rtext = reply["snippet"]["textDisplay"]
+                        #Save values in replies array
                         self.replies = self.replies + [[comment,author,rauthor,rtext]]
                     
 
         
-
+        #Check validity of URL
         try:
             video_id = urlparse(videoURL)
             q = parse_qs(video_id.query)
@@ -40,7 +42,7 @@ class YouTubeApi():
 
         except:
             print("Invalid YouTube URL")
-
+        #Set parameters for URL; e.g. https://www.googleapis.com/youtube/v3/commentThreads?key=AIzaSyDebkmzGWsRarwqzTQNOqbZ2Mbcprk_wE8&textFormat=plainText&part=snippet&videoId=kffacxfA7G4&maxResults=50 
         parms = {
                     'part': 'snippet,replies',
                     'videoId': vid,
@@ -50,26 +52,28 @@ class YouTubeApi():
                 }
 
         try:
-
+            #Test if there is next page in comment section and get nextPageToken; call load_comments first time
             matches = self.openURL(YOUTUBE_COMMENT_URL, parms)
             
             mat = json.loads(matches)
             nextPageToken = mat.get("nextPageToken")
-            
             load_comments(self)
-
+            
+            #While there is another page in comment section, parse JSON object in array and 
+            #call load_comments function which fills comment and replies arrays with values
             while nextPageToken:
                 parms.update({'pageToken': nextPageToken})
                 matches = self.openURL(YOUTUBE_COMMENT_URL, parms)
                 mat = json.loads(matches)
                 nextPageToken = mat.get("nextPageToken")            
                 load_comments(self)
-
+                
         except KeyboardInterrupt:
             print("User Aborted the Operation")
 
         except Exception as e:
-            print("Exception: ",e) 
+            print("YT Exception: ",e) 
+    #Open URL and get raw data from web service in JSON type
     def openURL(self, url, parms):
         f = urlopen(url + '?' + urlencode(parms))
         data = f.read()
